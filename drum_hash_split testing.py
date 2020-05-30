e2c265bd45251c626b425627b36c889fd4032a0a
Drake Champagne <trivorak@gmail.com>
2020-05-30 14:31:55 -0500

    Map and function testing
    added mapping defined functions.

--- drum_hash_split testing.py
+++ drum_hash_split testing.py
@@ -1,6 +1,6 @@
 #Default Startup/Test Values
 inCache = 'cd1a15ca9c8625415891632403b401f741fbe96e'
-splLen = 4          #Split Length
+splLen = 2         #Split Length
 instCou = 3         #Instrument Count
 percList = list()   #init var list
 
@@ -16,5 +16,21 @@ print(percList)
 for x in range(0,instCou):
   print(bin(int(percList[x],16)))
 
+# Replace hex with binary
+def hex2bin(hexVal):
+  return bin(int(hexVal,16))
+
+# Remove Binary Header
+def removebin(binVal):
+  return binVal[2:]
+
+# Fill Missing Binary Digits
+def binfntload(binVal):
+  return binVal.zfill(4*splLen)
 
 
+result = map(hex2bin,percList)
+result = map(removebin,result)
+result = map(binfntload,result)
+
+print(list(result))

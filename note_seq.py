5b5381ea4db71fcb983c5828be53d929103e41ff
Drake Champagne <trivorak@gmail.com>
2021-01-29 08:08:07 -0600

    Updated Note Sequence
    Removing the dependency from Sonic-Pi. Going to a more straight forwared Midi export process

--- note_seq.py
+++ note_seq.py
@@ -15,49 +15,8 @@ volume = 100
 MyMIDI = MIDIFile(1)
 MyMIDI.addTempo(track,time,tempo)
 
-
-#print(noteList)
-#print(len(noteList))
-
-
 def lookupNote(n):
-  if n == '0':
-    return 36
-  elif n == '1':
-    return 37
-  elif n == '2':
-    return 38
-  elif n == '3':
-    return 39
-  elif n == '4':
-    return 40
-  elif n == '5':
-    return 41
-  elif n == '6':
-    return 42
-  elif n == '7':
-    return 43
-  elif n == '8':
-    return 44
-  elif n == '9':
-    return 45
-  elif n == 'a':
-    return 46
-  elif n == 'b':
-    return 47
-  elif n == 'c':
-    return 48
-  elif n == 'd':
-    return 49
-  elif n == 'e':
-    return 50
-  elif n == 'f':
-    return 51
-  
-
-""" for x in range(0,len(noteList)):
-  play(lookupNote(noteList[x])+12)
-  sleep(0.25) """
+  return 36 + int(n,16)
   
 for x in range(0,len(noteList)):
   MyMIDI.addNote(track, channel, lookupNote(noteList[x]), time, duration, volume)

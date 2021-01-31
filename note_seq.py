3ae0033befbb32751caf67482756c68f5c44b56c
drake <trivorak@gmail.com>
2021-01-31 16:48:49 -0600

    Scale/Octave/Filename fix

--- note_seq.py
+++ note_seq.py
@@ -1,10 +1,13 @@
 from midiutil import MIDIFile
+import math
 
 #Default Startup/Test Values
+
 inCache = 'd3aca892d996d534c0fa98ae06d2a46e43836c8e'
 noteList = list(inCache)
-scaleList = [0,2,3,5,7,9,11,12]
+scaleList = [0,2,3,5,7,9,11]
 rootNote = 36
+midiFileName = inCache[:8]+"-m16.mid"
 
 track = 0
 channel = 0
@@ -19,20 +22,16 @@ MyMIDI.addTempo(track,time,tempo)
 def lookupNote(n):
   return int(n,16)
   
-# for x in range(0,len(noteList)):
-#     print(scaleList[lookupNote(noteList[x])%8]+rootNote)
-
 for x in range(0,len(noteList)):
   noteReturn = lookupNote(noteList[x])
-  scaleReturn = scaleList[noteReturn%8]
-
-  if scaleReturn != noteReturn:
-    rootNoteOffset = 12 + rootNote
-  else:
-    rootNoteOffset = rootNote
+  scaleReturn = scaleList[noteReturn%len(scaleList)]
+  
+  rootNoteOffset = (math.floor(noteReturn / len(scaleList))*12)+rootNote
 
   MyMIDI.addNote(track, channel, scaleReturn+rootNoteOffset, time, duration, volume)
   time = time + 1
-
-with open("trial_midi.mid", "wb") as output_file:
+  print(scaleReturn+rootNoteOffset)
+  
+with open(midiFileName, "wb") as output_file:
     MyMIDI.writeFile(output_file)
+

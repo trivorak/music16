c55eab529ba02d31d68a7a27ff887f0a693a57d9
Drake Champagne <trivorak@gmail.com>
2021-02-04 12:35:54 -0600

    updated drum_trig_test
    Added midi file export to the drum_trig_test.py file. Now exporting a midi drum file. Still static

--- drum_trig_test.py
+++ drum_trig_test.py
@@ -1,18 +1,33 @@
-from psonic import *
+from midiutil import MIDIFile
+
+track = 0
+channel = 0
+time = 0
+duration = .25
+tempo = 60
+volume = 100
+
+MyMIDI = MIDIFile(1)
+MyMIDI.addTempo(track,time,tempo)
+
 
 perc1 = list('11001011')
 perc2 = list('00110101')
 perc3 = list('00010101')
 
-for x in range(0, 40):
-  if int(perc1[x % len(perc1)]) == 1:
-    sample('kick.wav')
 
-  if int(perc2[x % len(perc2)]) == 1:
-   	sample('snare.wav')
 
-  if int(perc3[x % len(perc3)]) == 1:
-   	sample('chh.wav')
+for x in range(0, len(perc1)):
+  if int(perc1[x]) == 1:
+     MyMIDI.addNote(track, channel, 36, time, duration, volume)
 
-  sleep(.5)
+  if int(perc2[x]) == 1:
+   	MyMIDI.addNote(track, channel, 38, time, duration, volume)
 
+  if int(perc3[x]) == 1:
+   	MyMIDI.addNote(track, channel, 42, time, duration, volume)
+  time = time + .5
+
+
+with open("drums.mid", "wb") as output_file:
+    MyMIDI.writeFile(output_file)
